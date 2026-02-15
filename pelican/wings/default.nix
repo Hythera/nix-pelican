849f6f97413a6f7d3a730efea8bb0ffd52c18ea0
Hythera <87016780+Hythera@users.noreply.github.com>
2026-02-15 18:14:00 +0100

    pelican.wings: 1.0.0-beta23 -> 1.0.0-beta24

--- pelican/wings/default.nix
+++ pelican/wings/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "pelican-wings";
-  version = "1.0.0-beta23";
+  version = "1.0.0-beta24";
 
   src = fetchFromGitHub {
     owner = "pelican-dev";
     repo = "wings";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-guvcyZ8VLEEi2osbO/yO0aFQJTIppaFRCtxnaWv1qmM=";
+    hash = "sha256-MveNLXINvxAjJOG9nvXgfSxnEUkHI0Bnqxmgg/0Qu6Q=";
   };
 
   vendorHash = "sha256-juiJGX0wax1iIAODAgBUNLlfFg4kd14bB6IeEqohs8U=";
